80cdf965f770113e360dc1fa5979e07b55072bc2
qualidan <35110114+qualidan@users.noreply.github.com>
2018-07-05 15:25:18 -0700

    fixed dir for sysctl.conf
    /etc/sysctl.conf

--- start_metasploit.sh
+++ start_metasploit.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo /sbin/sysctl -w kernel.shmmax=134217728
-sudo chown ec2-user sysctl.conf
+sudo chown ec2-user /etc/sysctl.conf
 sudo echo "kernel.shmmax = 134217729" >> /etc/sysctl.conf
 sudo update-rc.d postgresql enable
 sudo service postgresql start
