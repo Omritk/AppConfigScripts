f0d058d4c0a3352c53d30f077e9dc1e52d939e7f
qualidan <35110114+qualidan@users.noreply.github.com>
2018-07-13 15:05:39 -0700

    openvas config
    changed

--- start_metasploit.sh
+++ start_metasploit.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
-sudo apt-get  --assume-yes install openvas
+sudo chown ec2-user /etc/apt/sources.list
+sudo echo "deb https://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install openvas
 sudo openvas-setup
 sudo openvas-start
+sudo openvasmd --rebuild
+sudo openvasmd --user=admin --new-password=csadmin
 sudo /sbin/sysctl -w kernel.shmmax=134217728
 sudo chown ec2-user /etc/sysctl.conf
 sudo echo "kernel.shmmax = 134217729" >> /etc/sysctl.conf
