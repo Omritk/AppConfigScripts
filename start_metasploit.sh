39abaa4e623c904b34668de922c3e150fe9cacc2
qualidan <35110114+qualidan@users.noreply.github.com>
2018-07-13 17:58:22 -0700

    openVAS commented out
    openVAS commented out

--- start_metasploit.sh
+++ start_metasploit.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-sudo chown ec2-user /etc/apt/sources.list
-sudo echo "deb https://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install openvas
+#sudo chown ec2-user /etc/apt/sources.list
+#sudo echo "deb https://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
+#sudo apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install openvas
 #sudo openvas-setup
-sudo openvas-start
+#sudo openvas-start
 #sudo openvasmd --rebuild
 #sudo openvasmd --user=admin --new-password=csadmin
 sudo /sbin/sysctl -w kernel.shmmax=134217728
